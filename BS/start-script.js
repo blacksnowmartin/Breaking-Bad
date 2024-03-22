31de308a79cc5598935f46b7e4e109f8a9b97893
blacksnowmartin <martinmwendwa005@gmail.com>
2024-03-22 21:14:51 +0300

    level code settled

---
+++ BS/start-script.js
@@ -0,0 +1,5 @@
+document.getElementById("new-game-btn").addEventListener("click", function() {
+    // Add code to start a new game here
+    // For example, you can redirect to the first level of the game
+    window.location.href = "level1.html"; // Redirect to the first level
+});
