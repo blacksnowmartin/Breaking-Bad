c571763882c70c7bb3b47d1effea3861fb7a3cd0
blacksnowmartin <martinmwendwa005@gmail.com>
2024-04-10 20:26:59 +0300

    Removed the Continue game feature for the sake of simplifying the game

--- Clean version/start.js
+++ Clean version/start.js
@@ -4,6 +4,12 @@ document.getElementById("new-game-btn").addEventListener("click", function() {
     window.location.href = "sn1.html"; // Redirect to the first level
 });
 
+document.getElementById("new-game-btn").addEventListener("click", function() {
+    // Add code to start a new game here
+    // For example, you can redirect to the first level of the game
+    window.location.href = "sn1.html"; // Redirect to the first level
+});
+
 document.getElementById("about-developer-btn").addEventListener("click", function() {
     // Add code to start a new game here
     // For example, you can redirect to the first level of the game
