2d48afe13a8fde0b54ae88dd1d99284408e1ec37
blacksnowmartin <martinmwendwa005@gmail.com>
2024-04-10 20:29:35 +0300

    added the instructions on how to play the game

--- Clean version/start.js
+++ Clean version/start.js
@@ -4,10 +4,9 @@ document.getElementById("new-game-btn").addEventListener("click", function() {
     window.location.href = "sn1.html"; // Redirect to the first level
 });
 
-document.getElementById("new-game-btn").addEventListener("click", function() {
-    // Add code to start a new game here
-    // For example, you can redirect to the first level of the game
-    window.location.href = "sn1.html"; // Redirect to the first level
+document.getElementById("instructions-btn").addEventListener("click", function() {
+    // Add code to show instructions on how to play the game here
+    window.location.href = "instructions.html"; // Redirect to the instructions
 });
 
 document.getElementById("about-developer-btn").addEventListener("click", function() {
